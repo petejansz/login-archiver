0b7a67d290a6116fe67530183c49a86782f6e458
Pete Jansz <peterjansz@gmail.com>
2020-10-16 12:10:14 -0700

    ca-imo-18036: To fix Unsubscribe host-events, use processor that decrypts token, strips leading '0' from playerId.  Also, in sec-gateway pws esa-configuration-secgateway.xml, create explicit resource for each promo/unsubscribe and unsub host-event.

--- purge-old-sms-reserve-contracts.sql
+++ purge-old-sms-reserve-contracts.sql
@@ -1,8 +1,11 @@
 -- Purge old sms_reserve_contracts
+-- RESERVE_STATUS_ID	NAME
+--                 1	REQUESTED
+--                 2	CONFIRMED
 
 delete
 (
-    select * from GMS4.sms_reserve_contracts where reserve_status_id=2 and last_updated < current date - 3 months
+    select * from GMS4.sms_reserve_contracts where reserve_status_id=1 and last_updated < current date - 3 months
     fetch first 1000 rows only
     with ur
 );
--- purge-old-sms-token-tb.sql
+++ purge-old-sms-token-tb.sql
@@ -1,8 +1,9 @@
 -- Purge old SMS_TOKEN_TB
+-- Purging policy based on STATUS_ID in (1,2,3) (validated, expired, invalidated) and older than 3-mos
 
 delete
 (
-    select * from GMS4.sms_token_tb where lastupdate_date < current date - 3 months
+    select * from GMS4.sms_token_tb where status_id in (1, 2, 3) and lastupdate_date < current date - 3 months
     fetch first 1000 rows only
     with ur
 );
